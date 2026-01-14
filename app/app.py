3d6e771120e27d18457db959a4781f75c08c38a0
Shubham Panwar <panwarshubham9315@gmail.com>
2026-01-14 08:35:34 +0530

    Add files via upload

---
+++ app/app.py
@@ -0,0 +1,61 @@
+import os
+import joblib
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+import streamlit as st
+import pandas as pd
+import joblib 
+
+# Deserializing the pickle files
+model = joblib.load('LogisticRegression_Heart.pkl')
+scaler = joblib.load('Scaler.pkl')
+expected_columns = joblib.load('Columns.pkl')
+
+st.title("Heart Stroke Prediction")
+st.markdown("Enter the following details")
+
+age = st.slider("Age", 18, 100, 40)
+sex = st.selectbox("Sex",['M','F'])
+chest_pain = st.selectbox("Chest Pain Type",['ATA','NAP','TA','ASY'])
+resting_bp = st.number_input("Resting Blood Pressure (mm Hg)", 80, 200, 120)
+cholesterol = st.number_input("Choleterol (mg/dL)", 100, 600, 200)
+fasting_bs = st.selectbox("Fasting Blood Sugar > 120 mg/dL", [0,1])
+resting_ecg = st.selectbox("Resting ECG", ['Normal','ST','LVH'])
+max_hr = st.slider("Maximum Heart Rate", 60, 220, 150)
+exercise_angina = st.selectbox("Exercise-Induced Angina",['Y','N'])
+old_peak = st.slider("Old Peak (ST Depression)", 0.0, 0.6, 0.1)
+st_slope = st.selectbox("ST Slope",['Up','Flat','Down'])
+
+if st.button("Predict"):
+    raw_input = {
+        'Age': age,
+        'RestingBP': resting_bp,
+        'Cholesterol': cholesterol,
+        'FastingBS': fasting_bs,
+        'MaxHR': max_hr,
+        'Oldpeak': old_peak,
+        'Sex_'+sex: 1,
+        'ChestPainType_'+chest_pain: 1,
+        'RestingECG_'+resting_ecg: 1,
+        'ExerciseAngina_'+exercise_angina: 1,
+        'ST_Slope_'+st_slope: 1,
+    }
+
+    input_df = pd.DataFrame([raw_input])
+
+    for col in expected_columns:
+        if col not in input_df:
+            input_df[col] = 0
+
+    input_df = input_df[expected_columns]
+
+    scaling_col = ['Age', 'RestingBP', 'Cholesterol', 'MaxHR', 'Oldpeak']
+    input_df[scaling_col] = scaler.transform(input_df[scaling_col])
+
+    prediction = model.predict(input_df)[0]
+
+    if prediction == 1:
+        st.error("High risk of Heart Stroke")
+    else:
+        st.success("Low risk of Heart Stroke")
